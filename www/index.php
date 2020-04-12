d31b26c14a671de0dcf507fbcd75d8b2bff5314c
Michal ŠMAHEL <48548230+ceskyDJ@users.noreply.github.com>
2020-04-13 00:39:05 +0200

    Added template for main page

--- www/index.php
+++ www/index.php
@@ -24,6 +24,138 @@ declare(strict_types = 1);
 
 
 <body>
+<header class="page-header">
+    <h1 class="main-heading">epůjčování.cz</h1>
 
+    <nav class="main-menu">
+        <ul class="menu-list">
+            <li class="menu-item"><a href="#" class="menu-link">Nabídka</a></li>
+            <li class="menu-item"><a href="#" class="menu-link">Podmínky</a></li>
+            <li class="menu-item"><a href="#" class="menu-link">Kontakty</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+<main class="page-content">
+    <header class="content-header">
+        <h2 class="content-heading">Nabídka aut k vypůjčení</h2>
+    </header>
+
+
+    <section class="content-section">
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+
+        <article class="rent-offer-article">
+            <header class="rent-offer-header">
+                <h3 class="rent-offer-heading">Open Bus</h3>
+            </header>
+
+            <section class="rent-offer-section">
+                <img src="img/36.png" alt="Ilustrační obrázek: Open Bus" class="car-image" />
+
+                <p class="car-price">Cena: 1000 Kč za den</p>
+
+                <a href="#" class="car-order">Objednat</a>
+            </section>
+        </article>
+    </section>
+</main>
 </body>
 </html>
