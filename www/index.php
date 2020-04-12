16421b18f2464839c60672d5060733226e5bec50
Michal ŠMAHEL <48548230+ceskyDJ@users.noreply.github.com>
2020-04-12 23:10:16 +0200

    Added file structure and base data

---
+++ www/index.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Index
+ *
+ * @author Michal Šmahel (ceskyDJ) <admin@ceskydj.cz>
+ * @copyright (C) 2020-now, Michal ŠMAHEL
+ */
+?>
+<!doctype html>
+
+
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Michal ŠMAHEL (ceskyDJ)" />
+    <title>ŠMAHEL - ePujcovani.cz</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+</head>
+
+
+<body>
+
+</body>
+</html>
